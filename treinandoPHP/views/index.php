b58aa250e1a3455dfaf1ecdf4d79737548771c8c
eliardo~ <eliardosilva81@gmail.com>
2022-03-08 17:20:54 -0300

    passando um array pra outra pagina como variavel global

--- treinandoPHP/views/index.php
+++ treinandoPHP/views/index.php
@@ -11,9 +11,6 @@
     <title>Document</title>
 </head>
 
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
 
 <body>
     <h1 class="text-center">treinando php</h1>
@@ -22,24 +19,28 @@
             <div class="card pl-2 pr-2 pb-3 pt-3 border">
                 <form name="formulario" method="POST" action="listProducts.php">
                     <?php
-                        session_start();
-                        $product = array("arroz", "feijao", "carne");
-                        $json = json_encode($product); 
+                    session_start();
+                    $_SESSION["product"] = array("arroz", "feijao", "carne");
+                    $json = json_encode($product);
                     ?>
                     <div class="form-group">
                         <label>Informe um produto novo</label>
-                        <input type="text" name="product" class="form-control">
+                        <input type="text" name="array[]" class="form-control">
                     </div>
                     <div class="form-group">
                         <label>Preço do Produto</label>
-                        <input type="number" name="productPrice" class="form-control">
+                       <input type="number" name="array[]" class="form-control">
                     </div>
                     <button type="submit" class="btn btn-primary rounded-pill">Ver produtos na proxima pagina</button>
-                    <input type="hidden" name="array" >">
+                    <input type="hidden" name="array[]" >
                 </form>
             </div>
         </div>
     </section>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
--- treinandoPHP/views/listProducts.php
+++ treinandoPHP/views/listProducts.php
@@ -10,9 +10,6 @@
     <title>Document</title>
 </head>
 
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
 
 <body>
     <h1 class="text-center">Produtos</h1>
@@ -21,6 +18,7 @@
             <div class="row">
                 <div class="col text-center mt-5">
                     <?php
+                    session_start();
                     //$nameProduct = isset($_POST['product']) ? $_POST['product'] : "";
                     //echo "produto: $nameProduct<br />";
 
@@ -30,18 +28,22 @@
                     //$array = isset($_GET['array'][0]) ? $_GET['array'][0] :$_GET['array'][1];
                     //echo "produtos do array $array";
                     //echo $array."<br>";
-                    $list = json_decode($_POST['array'], true);
+                    $list = $_POST['array'];
+                    $prod = $_SESSION["product"];
+                    // $array2 = isset($_GET['array[]']) ? $_GET['array[]'] : "nao chegou";
+                    // echo $array2;
 
-                   // $array2 = isset($_GET['array[]']) ? $_GET['array[]'] : "nao chegou";
-                   // echo $array2;
-
-                    foreach ($list as $values) {
+                    foreach ($prod as $values) {
                         echo $values;
                     }
                     ?>
                 </div>
             </div>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+
     </section>
 </body>
 
