1a1f5f69750512281d02ab305fc17656a633c9de
eliardo~ <eliardosilva81@gmail.com>
2022-03-08 09:35:57 -0300

    form passando dados para outra pagina

---
+++ treinandoPHP/views/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="./../styles/index.sass">
+    <title>Document</title>
+</head>
+
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+
+<body>
+    <h1 class="text-center">treinando php</h1>
+    <section>
+        <div class="container-fluid">
+            <div class="card pl-2 pr-2 pb-3 pt-3 border">
+            <form name="formulario" method="POST" action="listProducts.php">
+                <div class="form-group">
+                    <label>Informe um produto novo</label>
+                    <input type="text" name="product" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label>Preço do Produto</label>
+                    <input type="number" class="form-control" name="productPrice">
+                </div>
+                <button type="submit" class="btn btn-primary rounded-pill"
+                >Ver produtos na proxima pagina
+            </button> 
+            </form>
+            </div>
+        </div>
+    </section>
+
+</body>
+
+<style>
+
+</style>
+</html>
